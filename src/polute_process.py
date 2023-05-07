31d0f7c9ba274d537441c6f17585f813de826168
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-05-07 16:01:39 +0300

    a lot of uncategorised updates

--- src/polute_process.py
+++ src/polute_process.py
@@ -7,14 +7,15 @@ There will be next measures:
     length in meters.
 """
 
-
+from tqdm import tqdm
 import numpy as np
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 
 
-def get_drain_characteristic_function(u_critical: float=0):
+def get_drain_characteristic_function(u_critical: float):
     def my_function(u: float):
         if u < u_critical:
             return 0
@@ -22,6 +23,67 @@ def get_drain_characteristic_function(u_critical: float=0):
         return 1
         
     return my_function
+
+def _gaus_forward(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    A_cur = np.array(A, copy=True)
+    b_cur = np.array(b, copy=True)
+    
+    n = len(A_cur)
+
+    for j in range(n):
+        # if diagonal element is zero
+        if A_cur[j][j] == 0:
+            big = 0
+            k_row = j
+        
+            for k in range(j + 1, n):
+                if abs(A_cur[k][j]) > big:
+                    big = abs(A_cur[k][j])
+                    k_row = k
+            
+            for l in range(j, n):
+                A_cur[j][l], A_cur[k_row][l] = A_cur[k_row][l], A_cur[j][l]
+
+            b_cur[j], b_cur[k_row] = b_cur[k_row], b_cur[j]
+
+        pivot = A_cur[j][j]
+
+        # error case
+        if pivot == 0:
+            raise ValueError("Given matrix is singular")
+
+        # main part
+        for i in range(j + 1, n):
+            mult = A_cur[i][j] / pivot
+
+            for l in range(j, n):
+                A_cur[i][l] = A_cur[i][l] - mult * A_cur[j][l]
+            
+            b_cur[i] = b_cur[i] - mult * b_cur[j]
+        
+    return A_cur, b_cur
+
+def _gaus_backward(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    n = len(A)
+    X = np.zeros((n, 1))
+
+    for i in range(n-1, -1, -1):
+        sum = 0
+
+        for j in range(i+1, n):
+            sum += X[j] * A[i][j]
+        
+        X[i] = 1 / A[i][i] * (b[i] - sum)
+    
+    return X
+
+def gaus(A: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """Solve matrix equation
+    """
+    A, b = _gaus_forward(A=A, b=b)
+    X = _gaus_backward(A=A, b=b)
+
+    return X
 
 
 class PoluteProcessInspector:
@@ -40,15 +102,21 @@ class PoluteProcessInspector:
         self.k_2 = k_2
         self.dx = area_size[0] / partion[0]
         self.dy = area_size[1] / partion[1]
-        self.dt = 0.5 * pow(min(self.dx, self.dx), 2)
+        # self.dt = 0.5 * pow(min(self.dx, self.dy), 2)
+        self.dt = 1
         self.drain_ch = drain_characteristic
         self.sourse = sourse
+
+        self.sourse_points = []
 
         self._build_table()
 
         # # TODO: that part might be in the solvation function
         # self._tables = list()
         # self._tables.append(self.current_table())
+
+    def append_sourse_point(self, x: int, y: int) -> None:
+        self.sourse_points.append((x, y))
 
     def print_info(self)-> None:
         """Print all neseesary information for you
@@ -62,21 +130,22 @@ class PoluteProcessInspector:
         print(f"dx: {self.dx}")
         print(f"dy: {self.dy}")
         print(f"dt: {self.dt}")
+        print(f"sourse_points: {self.sourse_points}")
 
-    def _index_of(self, i: int, j: int) -> int:
-        if i < 0 or j < 0 or i >= self.partion[0] or j >= self.partion[1]:
-            raise ValueError(f"ERROR: indexes (i, j): ({i}, {j}) out of range, where range is ({self.partion})")
+    def _index_of(self, c: int, l: int) -> int:
+        if c < 0 or l < 0 or c >= self.partion[0] or l >= self.partion[1]:
+            raise ValueError(f"ERROR: indexes (c, l): ({c}, {l}) out of range, where range is ({self.partion})")
         
-        return j * self.partion[0] + i
+        return l * self.partion[0] + c
     
     def _double_index_of(self, index: int) -> list:
         if index < 0 or index > self.partion[0] * self.partion[1] - 1:
             raise ValueError(f"ERROR: index ({index}) out of range")
         
-        j = int(index / self.partion[0])
-        i = index - self.partion[0]* j
+        l = int(index / self.partion[0])
+        c = index - self.partion[0]* l
 
-        return i, j
+        return c, l
 
     def _get_start_table(self) -> np.ndarray:
         """Creates table for 0 time stamp and will be
@@ -92,13 +161,29 @@ class PoluteProcessInspector:
     def set_value(self, x: int, y: int, value: float) -> None:
         self.current_table[x, y] = value
 
+    def print_table_bitvise(self, table):
+        # print table
+        print("A::::")
+        for l in table:
+            print("[", end="")
+            for c in l:
+                print(f"{c:0.0f}", end="")
+            
+            print("],")
+
     def create_next_table_non_clear_scheme(self) -> None:
         A, b = self._create_diff_system()
+        # self.print_table_bitvise(table=A)
         X = self._solve_equation(A, b)
 
-        X = np.absolute(X)
+        X = np.reshape(X, self.partion)
 
-        self.current_table = self.current_table + np.reshape(X, self.partion)
+        # self.print_table_bitvise(X)
+
+        for p in self.sourse_points:
+            X[p[0], p[1]] += self.sourse * self.dt
+
+        self.current_table = X
 
     def create_next_table_clear_scheme(self) -> None:
         """Creates table on the next time point
@@ -108,18 +193,25 @@ class PoluteProcessInspector:
         
         for i in range(1, self.partion[0]-1):
             for j in range(1, self.partion[1]-1):
-                temp_table[i, j] = (1 - self.k_1 * 2 * self.dt / (self.dx * self.dx) - self.k_2 * 2 * self.dt / (self.dy * self.dy)) * self.current_table[i, j]\
-                    + self.k_1 * self.dt / (self.dx * self.dx) * (self.current_table[i+1, j] + self.current_table[i-1, j])\
-                    + self.k_2 * self.dt / (self.dy * self.dy) * (self.current_table[i, j+1] + self.current_table[i, j-1])\
+                temp_table[i, j] = (1 - self.k_1 * 2 * self.dt / pow(self.dx, 2) - self.k_2 * 2 * self.dt / pow(self.dy, 2)) * self.current_table[i, j]\
+                    + self.k_1 * self.dt / pow(self.dx, 2) * (self.current_table[i+1, j] + self.current_table[i-1, j])\
+                    + self.k_2 * self.dt / pow(self.dy, 2) * (self.current_table[i, j+1] + self.current_table[i, j-1])\
                     # - 1 / (2) * (self.current_table[i+1, j] * self.drain_ch(self.current_table[i+1, j]) - self.current_table[i-1, j] * self.drain_ch(self.current_table[i-1, j]))\
-                    # - 1 / (2) * (self.current_table[i, j+1] * self.drain_ch(self.current_table[i, j+1]) - self.current_table[i, j-1] * self.drain_ch(self.current_table[i, j-1]))
+                    # - 1 / (2) * (self.current_table[i, j+1] * self.drain_ch(self.current_table[i, j+1]) - self.current_table[i, j-1] * self.drain_ch(self.current_table[i, j-1]))\
+                    # + 0
 
-        self.current_table = temp_table
+        for p in self.sourse_points:
+            temp_table[p[0], p[1]] += self.sourse * self.dt
+
+        self.current_table = np.abs(temp_table)
 
     def print_curent_table(self) -> None:
         """Print curent table in the console
         """
-        print(self.current_table)
+        print("")
+        for l in self.current_table:
+            print(l)
+        # print(self.current_table)
 
     def plot_curent_table(self) -> None:
         """Plot curent table
@@ -127,33 +219,46 @@ class PoluteProcessInspector:
         self.plot_table(self.current_table)
 
     def plot_table(self, table: np.ndarray) -> None:
+        # self.plot_table_lined_graph(table)
+        self.plot_table_warm_map(table)
+
+    def plot_table_lined_graph(self, table: np.ndarray) -> None:
         """Plot gieven table
         """
-        ax = plt.figure().add_subplot(projection='3d')
-        # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
-        # Make data.
         X = np.arange(0, self.partion[0], 1)
         Y = np.arange(0, self.partion[1], 1)
         X, Y = np.meshgrid(X, Y)
         Z = table
+        
+        ax = plt.figure().add_subplot(projection='3d')
 
-        # Plot the surface.
-        surf = ax.plot_surface(X, Y, Z, linewidth=0)
+        # Plot the 3D surface
+        ax.plot_surface(X, Y, Z, edgecolor='royalblue', alpha=0.3)
 
-        # Plot projections of the contours for each dimension.  By choosing offsets
-        # that match the appropriate axes limits, the projected contours will sit on
-        # the 'walls' of the graph.
-        # ax.contour(X, Y, Z, zdir='z', offset=-100, cmap='coolwarm')
-        # ax.contour(X, Y, Z, zdir='x', offset=-40, cmap='coolwarm')
-        # ax.contour(X, Y, Z, zdir='y', offset=40, cmap='coolwarm')
+        ax.set(xlim=(-2, self.partion[0] + 2), ylim=(-2, self.partion[1] + 2), zlim=(-2, 17),
+            xlabel='X', ylabel='Y', zlabel='Z')
 
-        # ax.set(xlim=(0, 20), ylim=(0, 20), zlim=(-10, 10),
-        #     xlabel='X', ylabel='Y', zlabel='Z')
+        plt.show()
+    
+    def plot_table_warm_map(self, table: np.ndarray) -> None:
+        """Plot gieven table
+        """
+        plt.style.use('_mpl-gallery-nogrid')
+
+        X = np.arange(0, self.partion[0], 1)
+        Y = np.arange(0, self.partion[1], 1)
+        X, Y = np.meshgrid(X, Y)
+        Z = table
+        
+        # fig, ax = plt.subplots()
+
+        plt.figure(figsize=(7,6))
+        plt.contourf(X, Y, Z)
+        plt.colorbar()
 
         plt.show()
 
-    def _create_diff_system(self, l_val: float=0.5) -> np.ndarray:
+    def _create_diff_system(self, l_val: float=0.7) -> np.ndarray:
         """TODO: write comment
         """
         n = self.partion[0] * self.partion[1]
@@ -168,103 +273,76 @@ class PoluteProcessInspector:
         # fill lines corresponded to border points
         for i in range(self.partion[0]):
             for j in [0, self.partion[1] - 1]:
-                A[iof(i=i, j=j), iof(i=i, j=j)] = 1
+                A[iof(c=i, l=j), iof(c=i, l=j)] = 1
         
         for i in [0, self.partion[0] - 1]:
             for j in range(self.partion[1]):
-                A[iof(i=i, j=j), iof(i=i, j=j)] = 1
+                A[iof(c=i, l=j), iof(c=i, l=j)] = 1
 
         # Fill central elements
+        # i is Oy partion, j is Ox partion
         for i in range(1, self.partion[0] - 1):
             for j in range(1, self.partion[1] - 1):
-                cur_line = iof(i=i, j=j)
+                cur_line = iof(c=i, l=j)
                 
-                b[cur_line] = l_val * self.dt * ((ct[i-1, j] - 2*ct[i, j] + ct[i+1, j])/(self.dx * self.dx)\
-                    + (ct[i, j-1] - 2*ct[i, j] + ct[i, j+1])/(self.dy * self.dy))\
-                    + ct[i, j] + ct[i, j] * self.drain_ch(ct[i, j]) * self.dt + self.sourse * self.dt
+                b[cur_line] = l_val * self.dt * (\
+                      (ct[i-1, j] - 2 * ct[i, j] + ct[i+1, j]) / (pow(self.dx, 2))\
+                    + (ct[i, j-1] - 2 * ct[i, j] + ct[i, j+1]) / (pow(self.dy, 2))\
+                    ) + ct[i, j] + ct[i, j] * self.drain_ch(ct[i, j]) * self.dt
 
-                A[cur_line, iof(i=i, j=j)] = 1 + self.drain_ch(ct[i, j]) * self.dt + 2 * (1 - l_val) * self.dt * (self.k_1/pow(self.dx, 2) + self.k_1/pow(self.dy, 2))
-                A[cur_line, iof(i=i-1, j=j)] = (1 - l_val) * self.k_1 * self.dt / pow(self.dx, 2)
-                A[cur_line, iof(i=i+1, j=j)] = A[cur_line, iof(i=i-1, j=j)]
-                A[cur_line, iof(i=i, j=j-1)] = (1 - l_val) * self.k_2 * self.dt / pow(self.dy, 2)
-                A[cur_line, iof(i=i, j=j+1)] = A[cur_line, iof(i=i, j=j-1)]
+                A[cur_line, iof(c=i, l=j)] = 1 + self.drain_ch(ct[i, j]) * self.dt\
+                      + 2 * (1 - l_val) * self.dt * (self.k_1 / pow(self.dx, 2) + self.k_1 / pow(self.dy, 2))
+                A[cur_line, iof(c=i-1, l=j)] = - (1 - l_val) * self.k_1 * self.dt / pow(self.dx, 2)
+                A[cur_line, iof(c=i+1, l=j)] = A[cur_line, iof(c=i-1, l=j)]
+                A[cur_line, iof(c=i, l=j-1)] = - (1 - l_val) * self.k_2 * self.dt / pow(self.dy, 2)
+                A[cur_line, iof(c=i, l=j+1)] = A[cur_line, iof(c=i, l=j-1)]
 
         return A, b
 
     def _solve_equation(self, A: np.ndarray, b: np.ndarray) -> np.ndarray:
         """If X that AX = b exists, then return X
         """
-        # TODO: write my own algorithm
-        X = np.linalg.solve(A, b)
+        # X = np.linalg.solve(A, b)  // TODO: delete someones code
+        X = gaus(A=A, b=b)
         
         return X
 
-    def _forvard_gaus(self, matrix: np.ndarray) -> np.ndarray:
-        """ first step in gaus algorithm.
-        Args:
-            TODO: write the comment
+    
 
-        Returns:
-            np.ndarray: matrix with zeros below diagonal line 
-        """
-        # TODO: rewrite old code
-        if matrix.shape()[0] != matrix.shape()[1]:
-            raise ValueError(f"Matrix must be square but {matrix.shape()} was gieven")
-
-        temp_matrix = np.array(matrix, copy=True)
-        places = [i for i in range(matrix)]
-
-        for i in range(min(min(self.partion) + 1)):  # i - is column id, but will be used as diagonal index
-            if temp_matrix[i][i] == 0:
-                l = i + 1
-                while temp_matrix[i][i] == 0 and l < len(temp_matrix):
-                    if temp_matrix[l][i] == 1:
-                        places[i], places[l] = places[l], places[i]
-                        temp_matrix[i], temp_matrix[l] = temp_matrix[l], temp_matrix[i]
-
-                        # for c in range(i, len(temp_matrix[0])):
-                        #     temp_matrix[i][c] = (temp_matrix[l][c] + temp_matrix[i][c]) % 2
-
-                    l += 1
-
-            for l in range(i+1, len(temp_matrix)):
-                if temp_matrix[l][i] == 1:
-                    for c in range(i, len(temp_matrix[0])):
-                        temp_matrix[l][c] = (temp_matrix[l][c] + temp_matrix[i][c]) % 2
-
-        return temp_matrix, places
+    
 
 
 def predict(len: int=10, skip: int=10) -> None:
-    area_size = np.array([1, 1])
-    # partion = 4, 4
+    area_size = np.array([50, 50])  # works up to 10
     partion = 20, 20
+    # partion = 30, 30
     k_1 = 10
     k_2 = 10
     u_cr = 12
     drain_characteristic = get_drain_characteristic_function(u_critical=u_cr)
     sourse = 5
 
-    John = PoluteProcessInspector(area_size=area_size, partion=partion,
-                                  k_1=k_1, k_2=k_2,
+    John = PoluteProcessInspector(area_size=area_size, partion=partion, k_1=k_1, k_2=k_2,
                                   drain_characteristic=drain_characteristic, sourse=sourse)
     
+    John.append_sourse_point(x=5, y=5)
+    John.append_sourse_point(x=7, y=7)
+    
     John.print_info()
-    John.set_value(x=5, y=5, value=sourse)
-    John.set_value(x=8, y=8, value=sourse)
-    # John.set_value(x=1, y=1, value=sourse)
-    # John.set_value(x=2, y=2, value=sourse)
 
+    John.create_next_table_non_clear_scheme()
     John.plot_curent_table()
+    # John.print_curent_table()
     
     for i in range(len):
-        for s in range(skip):
+        for s in tqdm(range(skip)):
             John.create_next_table_non_clear_scheme()
 
         John.plot_curent_table()
+        # John.print_curent_table()
 
 def main():
-    predict(len=10, skip=10)
+    predict(len=10, skip=30)
 
 if __name__ == "__main__":
     main()
